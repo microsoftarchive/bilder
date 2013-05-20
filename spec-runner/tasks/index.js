715c4549f96135dc161043d5050133e5592d7ba3
Aditya <aditya@netroy.in>
2013-05-20 03:11:57 +0200

    spec runner generates coverage report too now.. yay for istanbul

--- spec-runner/tasks/index.js
+++ spec-runner/tasks/index.js
@@ -2,13 +2,17 @@ module.exports = function (grunt) {
 
   'use strict';
 
+  var vm = require('vm');
+  var fs = require('fs');
   var path = require('path');
+
   var jsdom = require('jsdom');
   var jquery = require('jquery');
   var chai = require('chai');
   var sinon = require('sinon');
   var Mocha = require('mocha');
   var requirejs = require('requirejs');
+  var istanbul = require('istanbul');
 
   var define = requirejs.define;
   var _ = grunt.util._;
@@ -20,6 +24,43 @@ module.exports = function (grunt) {
   function noop() { return {}; }
   function fakeLoader(a, b, load) { load(noop); }
 
+  // Instrument files for coverage
+  var oldRequireJSLoader = requirejs.load;
+  var makeNodeWrapper = requirejs.makeNodeWrapper;
+  var exists = fs.existsSync || path.existsSync;
+  function instrumentModule (options) {
+
+    return function (context, moduleName, url) {
+
+      options.files = options.files || [];
+
+      // skip files not marked for coverage & other plugin schtuff
+      if(options.files.indexOf(url) === -1 || !exists(url)) {
+        return oldRequireJSLoader.call(requirejs, context, moduleName, url);
+      }
+
+      // Load file from FS,
+      var contents = fs.readFileSync(url, 'utf8');
+
+      // Instrument the code
+      var instrumenter = new istanbul.Instrumenter();
+      contents = instrumenter.instrumentSync(contents, url);
+
+      // Wrap it for node.js
+      contents = makeNodeWrapper(contents);
+
+      // execute it in the context of requirejs
+      try {
+        vm.runInThisContext(contents, fs.realpathSync(url));
+      } catch (e) {
+         throw new Error('Failed loading module "' + moduleName + '" with error: ' + e);
+      }
+
+      // mark module as loaded
+      context.completeLoad(moduleName);
+    };
+  }
+
   // patch the context with some globals & stuff
   function patchMochaContext (mocha) {
 
@@ -28,7 +69,7 @@ module.exports = function (grunt) {
       // use a fresh new dom for every test
       var win = jsdom.jsdom().createWindow('<!doctype html><body/>');
       win.navigator = context.navigator = {
-        'userAgent': 'Wunderlist Test Runner',
+        'userAgent': 'Bilder Test Runner',
         'appVersion': '1.0.0'
       };
 
@@ -48,6 +89,7 @@ module.exports = function (grunt) {
 
         // make "requirejs" a global in specs running in nodejs
         ctx.requirejs = ctx.require = requirejs;
+        ctx.nodeRequire = require;
 
         // make chai functions available
         ctx.should = chai.should();
@@ -71,7 +113,6 @@ module.exports = function (grunt) {
 
         // Specs are on travis
         ctx.isTravis = isTravis;
-
       }
 
       // fix the main suite context first
@@ -89,14 +130,18 @@ module.exports = function (grunt) {
   grunt.registerTask('specs/mocha', 'Node based spec-runner for mocha', function () {
 
     var options = this.options({
-      'base': 'public',
+      'base': '',
       'glob': '**/*.spec.js',
       'ui': 'bdd',
       'reporter': 'spec',
       'globals': ['_', '$'],
+      'require': {
+        'base': 'public'
+      },
       'mocks': {},
       'fake_plugins': [],
-      'fake_modules': []
+      'fake_modules': [],
+      'coverage': {}
     });
 
     // Stub requirejs plugins
@@ -118,36 +163,77 @@ module.exports = function (grunt) {
     // Allow certain globals in mocha
     mocha.globals(options.globals);
 
-    // Run Mocha as BDD
+    // Configure Mocha UI & Reporter
     mocha.ui(options.ui);
     mocha.reporter(options.reporter);
 
     // Make mock paths absolute
-    var mockPaths = options.mocks;
-    Object.keys(mockPaths).forEach(function(name) {
-      mockPaths[name] = path.join(__dirname, '..', mockPaths[name]);
-    });
+    var mocks = options.mocks || {};
+    var paths = mocks.paths || {};
+    if(mocks.base && paths) {
+      Object.keys(paths).forEach(function(name) {
+        paths[name] = path.resolve(options.base, mocks.base, paths[name]);
+      });
+    }
 
     // find modules in the app folder
     requirejs.config({
-      'baseUrl': path.resolve(options.base),
-      'paths': mockPaths
+      'baseUrl': path.resolve(options.base, options.require.base),
+      'paths': paths
     });
+
+    // Make paths absolute for files marked for coverage
+    if(options.coverage.files) {
+      var globRules = options.coverage.files;
+      var files = [];
+      globRules.forEach(function(rule) {
+        rule = path.resolve(options.base, options.require.base, rule);
+        files.push.apply(files, glob.sync(rule));
+      });
+      options.coverage.files = files;
+    }
+
+    // Override requirejs.load for coverage generation
+    if (options.coverage.files) {
+      requirejs.load = instrumentModule(options.coverage);
+    }
 
     // Path the context
     patchMochaContext(mocha);
 
     // populate files
-    var globRule = path.join(options.base, options.glob);
+    var globRule = path.resolve(options.base, options.glob);
     mocha.files = glob.sync(globRule);
+
+    // if args contain a spec file name, just use that file alone
+    if(this.args.length && mocha.files.length) {
+      var specFile = this.args[0];
+      mocha.files = mocha.files.filter(function (file) {
+        return file.match(specFile);
+      });
+    }
 
     // Run it
     mocha.run(function (count) {
+
+      // Stop fataly on any failed specs
       if(count) {
         grunt.fatal(count + ' failures.');
-      } else {
-        done();
+      } else if (global.__coverage__) {
+
+        // Process the coverage
+        var collector = new istanbul.Collector();
+        collector.add(global.__coverage__);
+
+        // Generate the report
+        ['text-summary', 'html'].forEach(function (type) {
+          istanbul.Report.create(type, {
+            'dir': options.coverage.output_dir || ''
+          }).writeReport(collector, true);
+        });
       }
+
+      done();
     });
 
   });
